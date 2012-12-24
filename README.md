1fa1e61608a3b3cc44bf61423d3c4fdc8184d03b
Richard O'Dwyer <richard@richard.do>
2012-12-24 22:20:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+NGINX access log parser
+================================
+
+This python script parses an NGINX access log and counts the total occurrences of a chosen item within the logs. In the example.log it processes the requested file, this can be changes to any other segment of the log.
+
+The output is useful when serving media assets as you can serve assets from source and calculate view counts periodically from the NGINX logs using this parser.
+
+Note: if you log files are not in standard format the find() function will need editing accordingly.
---
+++ main.py
@@ -0,0 +1,54 @@
+__author__ = "Richard O'Dwyer"
+__email__ = "richard@richard.do"
+__license__ = "None"
+
+import re
+
+def process_log(log):
+    requests = get_requests(log)
+    files = get_files(requests)
+    totals = file_occur(files)
+    return totals
+
+def get_requests(f):
+    log_line = f.read()
+    pat = (r''
+           '(\d+.\d+.\d+.\d+)\s-\s-\s' #IP address
+           '\[(.+)\]\s' #datetime
+           '"GET\s(.+)\s\w+/.+"\s\d+\s' #requested file
+           '\d+\s"(.+)"\s' #referrer
+           '"(.+)"' #user agent
+        )
+    requests = find(pat, log_line, None)
+    return requests
+
+def find(pat, text, match_item):
+    match = re.findall(pat, text)
+    if match:
+        return match
+    else:
+        return False
+
+def get_files(requests):
+    #get requested files with req
+    requested_files = []
+    for req in requests:
+        #req[2] for req file match, change to
+        #data you want to count totals
+        requested_files.append(req[2])
+    return requested_files
+
+def file_occur(files):
+    #file occurrences in requested files
+    d = {}
+    for file in files:
+        d[file] = d.get(file,0)+1
+    return d
+
+if __name__ == '__main__':
+
+    #nginx access log, standard format
+    log_file = open('example.log', 'r')
+
+    #return dict of files and total requests
+    print(process_log(log_file))
